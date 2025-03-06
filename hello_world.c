db36b5292c910a08d8a499801efab8156937efee
esvkat15 <matthewhmunoz@gmail.com>
2025-03-06 10:31:05 -0800

    added LED blink example on GPIO pin 16

--- hello_world.c
+++ hello_world.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
 int main() {
     stdio_init_all();
+    gpio_init(16);
+    gpio_set_dir(16, GPIO_OUT);
     //setup_default_uart();
     while(1) {
         
         printf("Hello, world! 2\n");
         sleep_ms(1000);
+        gpio_put(16, 1);
+        sleep_ms(1000);
+        gpio_put(16, 0);
 
     }
     return 0;
